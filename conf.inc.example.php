be84587bef56a7314c6fb1824ea82066da11e186
LucdoOf <lucasg.18@hotmail.fr>
2019-09-02 22:25:23 +0200

    Added conf example

---
+++ conf.inc.example.php
@@ -0,0 +1,16 @@
+<?php
+define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/'));
+define('TWITTER_API_KEY', "QSnRjZLpgaeyEa6uDj0NgNn7N");
+define('TWITTER_API_KEY_SECRET', "C9uyAEljbZGd8OYi30WxTAznYiwV7ZvH0nnYVBVdaR8F5SEgRo");
+define('TWITTER_ACCESS_TOKEN', "1162384627719446528-jsUcgDTzRoFgbyNN3rzmhcrJarTK5r");
+define('TWITTER_ACCESS_TOKEN_SECRET', "uO4AepLT1Yjk0gv2ErbmL8wnSMExm4lONHFhLlm5j1a3O");
+define('FACEBOOK_APP_ID', 517793025693665);
+define('FACEBOOK_PAGE_ID', 109412767101979);
+define('FACEBOOK_APP_SECRET', "7d905d00cdd237b0423e10f428339b12");
+define('FACEBOOK_ACC_TOKEN', "EAAHW7hVdFZBEBAIkdy4deAXemhB9Lu7vTPyKYmye98ZB5ZBZBrxTClGWoadSYKc9vAzhKumjpeAZAe0YcAYR3iZAIJUd1d4wNRfF46tmaEtrs7CaVZA05j0IAGGH2g5JYZBHJ4mfYxBD9VOnzUy03JaQKLtSsjZBSVJ6HIOAfoQLFvn5HoZB67yBMC");
+define('TYNYMCE_API_KEY', '0tvtg1h3mktjq671wxjturc5t4das4zeiwl8b230fmlir9xe');
+define('ABSOLUTE_PATH', "https://www.geopole.net");
+define('IS_DEV', true);
+define('LOGO_SVG_PATH', ABSOLUTE_PATH . "/res/images/svg/logo.svg");
+define('LOGO_PNG_PATH', ABSOLUTE_PATH . "/res/images/svg/logo.png");
+define('LOGO_AMP_PATH', ABSOLUTE_PATH . "/res/images/amp-logo.png");
